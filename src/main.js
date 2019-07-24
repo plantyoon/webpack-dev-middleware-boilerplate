78703bf4f49df780bcb92e907afbd3724f0006a1
YY <colond@jetarrow.net>
2019-07-24 12:33:09 +0900

    init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider, connect } from 'react-redux'
+
 import './main.scss'
 
-console.log('App start!')
-// test
+class App extends React.Component {
+  render() {
+    return <div id="app">
+      <h1>Hello React!</h1>
+    </div>
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
